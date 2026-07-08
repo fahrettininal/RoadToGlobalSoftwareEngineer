5e519a16e0e9eef6629513d58a32bb8e119de5db
Fahrettin İnal <inalfahrettin65@gmail.com>
2026-07-08 20:42:12 +0300

    Day 3 completed Variables, data types and calculator project

---
+++ 01_Python/Day03/day03_calculator.py
@@ -0,0 +1,28 @@
+name = input("Enter your name:")
+
+print()
+print("Welcome:", name)
+print()
+
+print("==================================")
+print(" SIMPLE CALCULATOR")
+print("==================================")
+
+number1 = int(input("Enter first number:"))
+number2 = int(input("Entersecond number:"))
+
+print()
+print("RESULTS")
+print("==================================")
+print("Addition:", number1+number2)
+print("Subraction:", number1-number2)
+print("Multiplication:", number1*number2)
+print("Division:", number1/number2)
+print("Floor Division:", number1//number2)
+print("Ramainder:", number1%number2)
+print("Power:", number1**number2)
+
+print("==================================")
+
+print()
+print("Thank you for using the calculator!")
---
+++ 01_Python/Day03/day03_numbers.py
@@ -0,0 +1,31 @@
+age = 411
+height = 1.75
+salary = 45000.00
+is_programmer = True
+
+print(type(age))
+print(type(height))
+print(type(salary))
+print(type(is_programmer))
+
+print("Age        :", age)
+print("Height     :", height)
+print("Salary     :", salary)
+print("Programmer :", is_programmer)
+
+number1 = 20
+number2 = 5
+
+print("================================")
+print("MATEMATICS")
+print("================================")
+print("Addition:", number1+number2)
+print("Subraction:", number1-number2)
+print("Multiplication:", number1*number2)
+print("Division:", number1/number2)
+print("Floor Division:", number1//number2)
+print("Remainder:", number1%number2)
+print("Power:", number1**number2)
+
+   
+
