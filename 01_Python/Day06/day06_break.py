f9a735c85c8cf71c8c6977259d324f03abae4df9
Fahrettin İnal <inalfahrettin65@gmail.com>
2026-07-20 15:08:14 +0300

    Day06 completed

---
+++ 01_Python/Day06/day06_break.py
@@ -0,0 +1,16 @@
+print("========================")
+print("BREAK")
+print("=========================")
+
+counter = 1
+
+while True:
+    print(counter)
+    counter+=1
+
+    if counter == 6:
+        break
+
+print("Finished")
+
+
---
+++ 01_Python/Day06/day06_challenge.py
@@ -0,0 +1,16 @@
+print("===========================")
+print("TURNSTILE LOGIN")
+print("===========================")
+
+
+card = ""
+
+
+while card != "12345":
+   card = input("Wrong card:")
+
+
+if card != "12345":
+    
+    print("Access Granted")
+print("Welcome")
