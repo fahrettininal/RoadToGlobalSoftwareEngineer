2a5d694ec49b398fde42632cbd258e9453f02035
Fahrettin İNAL <fahrettininal@istanbul.gov.tr>
2026-07-21 16:18:05 +0300

    Day06 completed

---
+++ 01_Python/Day06/day06_continue.py
@@ -0,0 +1,14 @@
+print("===========================")
+print("Challenge1 continue")
+print("=============================")
+
+counter = 0
+
+while counter < 10:
+    counter += 1
+
+    if counter == 5:
+        continue
+    print(counter)
+
+
---
+++ 01_Python/Day06/day06_while_true.py
@@ -0,0 +1,16 @@
+print("=====================")
+print(" While TRUE ")
+print("======================")
+
+while True:
+    text = input("Write something (exit to quit): ")
+
+    if text == "exit":
+        break
+
+    print("You wrote:", text)
+
+print()
+print("Program Finished.")
+
+
